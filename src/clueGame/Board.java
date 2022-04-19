bc53e8cd803d8637cd57d2d9259c8f727bd55250
lukepowers30 <lukepowers@mines.edu>
2022-04-18 19:50:44 -0600

    Cleaned up the mouseListener in Board

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -556,28 +556,16 @@ public class Board extends JPanel{
 		}
 
 		@Override
-		public void mousePressed(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
+		public void mousePressed(MouseEvent e) {}
 
 		@Override
-		public void mouseReleased(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
+		public void mouseReleased(MouseEvent e) {}
 
 		@Override
-		public void mouseEntered(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
+		public void mouseEntered(MouseEvent e) {}
 
 		@Override
-		public void mouseExited(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
+		public void mouseExited(MouseEvent e) {}
 		
 	}
 
